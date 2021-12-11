cd70d43618481c1216540eb049990df308bbd821
Connor Hollis <connorhollis3d@gmail.com>
2021-12-11 16:50:52 -0700

    added first question for what is the project title

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [];
+
+const promptProject = (questions) => {
+    if (!questions) {
+        questions = [];
+    }
+    
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'projectTitle',
+            message: 'What is your Project Title? (Required)',
+            validate: name => {
+                if(name){
+                    return true
+                }else{
+                    console.log('Please enter Project Title!');
+                    return false;
+                }
+            }
+        }
+    ]).then(projectData => {
+        console.log(projectData)
+        
+        questions.push(projectData);
+            if (projectData.confirmAddProject) {
+                return promptProject(portfolioData);
+            } else {
+                return questions;
+        }
+    });
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -10,4 +42,5 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 // Function call to initialize app
+promptProject();
 init();
